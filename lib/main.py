60222d50962231d463ee2548da717126f4234c38
Maderu-CM <clive.moyia@student.moringaschool.com>
2023-09-07 01:35:30 +0300

    Assignment Done

---
+++ lib/main.py
@@ -0,0 +1,7 @@
+from countdown import countdown, countdown_with_sleep
+
+if __name__ == "__main__":
+    print("Countdown without sleep:")
+    countdown(5)  # Test countdown without sleep
+    print("\nCountdown with sleep:")
+    countdown_with_sleep(5)  # Test countdown with sleep
